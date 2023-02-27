813d8f8cbc04986f4191b2a2c63c526defc18780
ryn32233 <rmefford1@gmail.com>
2023-02-27 16:26:14 -0500

    add spell model

---
+++ app/src/main/java/com/example/dudesanddice/model/Spell.kt
@@ -0,0 +1,14 @@
+package com.example.dudesanddice.model
+
+data class Spell(
+
+    val spellName: String,
+    val spellLevel: Int,
+    val spellType: String,
+    val spellCastTime: String,
+    val spellRange: String,
+    val spellComponents: String,
+    val spellDuration: String,
+    val spellDescription: String
+
+    )
